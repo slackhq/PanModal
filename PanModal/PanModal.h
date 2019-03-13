b1c2d820291bbc145c187e98721c0e1856b7b441
Tosin Afolabi <tosman9000@yahoo.com>
2019-03-13 15:54:44 -0700

    Progress on Carthage Support.

---
+++ PanModal/PanModal.h
@@ -0,0 +1,19 @@
+//
+//  PanModal.h
+//  PanModal
+//
+//  Created by Tosin A on 3/13/19.
+//  Copyright © 2019 Detail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PanModal.
+FOUNDATION_EXPORT double PanModalVersionNumber;
+
+//! Project version string for PanModal.
+FOUNDATION_EXPORT const unsigned char PanModalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PanModal/PublicHeader.h>
+
+
