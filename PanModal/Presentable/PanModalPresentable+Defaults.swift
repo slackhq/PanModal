dffcb8529cb6b7362c52bcb916e6d783ae36c667
Stephen Sowole <tunsow@hotmail.com>
2019-03-19 19:07:15 -0700

    Update PanModalPresentable+Defaults.swift

--- PanModal/Presentable/PanModalPresentable+Defaults.swift
+++ PanModal/Presentable/PanModalPresentable+Defaults.swift
@@ -81,7 +81,7 @@ public extension PanModalPresentable where Self: UIViewController {
     }
 
     func shouldRespond(to panModalGestureRecognizer: UIPanGestureRecognizer) -> Bool {
-        return false
+        return true
     }
 
     func willRespond(to panModalGestureRecognizer: UIPanGestureRecognizer) {
