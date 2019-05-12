b8f7e613ec84897f9c01fbe03a119a1cc674daa4
tun57 <tunsow@hotmail.com>
2019-05-11 18:34:10 -0700

    Update PanModalTests.swift

--- Tests/PanModalTests.swift
+++ Tests/PanModalTests.swift
@@ -59,6 +59,8 @@ class PanModalTests: XCTestCase {
         XCTAssertEqual(vc.showDragIndicator, false)
         XCTAssertEqual(vc.shouldRoundTopCorners, false)
         XCTAssertEqual(vc.cornerRadius, 8.0)
+        XCTAssertEqual(vc.transitionDuration, PanModalAnimator.Constants.defaultTransitionDuration)
+        XCTAssertEqual(vc.transitionAnimationOptions, [.curveEaseInOut, .allowUserInteraction, .beginFromCurrentState])
     }
 
     func testPresentableYValues() {
