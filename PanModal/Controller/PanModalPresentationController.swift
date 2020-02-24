f523bb55fd6deccbb8109ab37e590e4ce9d13860
Stephen Sowole <ssowole@slack-corp.com>
2020-02-24 14:29:33 -0800

    [PanModal] Round frame positions for comparison

--- PanModal/Controller/PanModalPresentationController.swift
+++ PanModal/Controller/PanModalPresentationController.swift
@@ -311,7 +311,7 @@ private extension PanModalPresentationController {
     var isPresentedViewAnchored: Bool {
         if !isPresentedViewAnimating
             && extendsPanScrolling
-            && presentedView.frame.minY <= anchoredYPosition {
+            && presentedView.frame.minY.rounded() <= anchoredYPosition.rounded() {
             return true
         }
 
