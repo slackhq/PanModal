b2f5bd7d169cba564d5c5b7203864f79be3fe826
Jordan Pichler <j.pichler@icloud.com>
2020-04-21 20:16:30 +0200

    Add completion block to present function (#94)

--- PanModal/Presenter/PanModalPresenter.swift
+++ PanModal/Presenter/PanModalPresenter.swift
@@ -29,7 +29,10 @@ protocol PanModalPresenter: AnyObject {
     /**
      Presents a view controller that conforms to the PanModalPresentable protocol
      */
-    func presentPanModal(_ viewControllerToPresent: PanModalPresentable.LayoutType, sourceView: UIView?, sourceRect: CGRect)
+    func presentPanModal(_ viewControllerToPresent: PanModalPresentable.LayoutType,
+                         sourceView: UIView?,
+                         sourceRect: CGRect,
+                         completion: (() -> Void)?)
 
 }
 #endif
