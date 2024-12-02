b91453f5609a91978fd149e29929d5550c5ed174
Andrey Rybalkin <andreysavie@ya.ru>
2024-12-02 10:11:10 +0300

    Fix ios 17.1+ issue

--- PanModal/Controller/PanModalPresentationController.swift
+++ PanModal/Controller/PanModalPresentationController.swift
@@ -177,9 +177,14 @@ open class PanModalPresentationController: UIPresentationController {
         guard let containerView = containerView
             else { return }
 
+        if self.panContainerView.frame == .zero {
+             self.adjustPresentedViewFrame()
+         }
+        
         layoutBackgroundView(in: containerView)
         layoutPresentedView(in: containerView)
         configureScrollViewInsets()
+        
 
         guard let coordinator = presentedViewController.transitionCoordinator else {
             backgroundView.dimState = .max
