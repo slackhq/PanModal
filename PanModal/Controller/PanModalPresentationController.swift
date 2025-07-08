b2b717624c1adac7a49c03e87c474dfcd49a7846
achauhan-legion <achauhan@legion.co>
2025-07-08 11:38:28 +0530

    bottom sheet fix

--- PanModal/Controller/PanModalPresentationController.swift
+++ PanModal/Controller/PanModalPresentationController.swift
@@ -177,6 +177,10 @@ open class PanModalPresentationController: UIPresentationController {
         guard let containerView = containerView
             else { return }
 
+        if self.panContainerView.frame == .zero {
+            self.adjustPresentedViewFrame()
+        }
+        
         layoutBackgroundView(in: containerView)
         layoutPresentedView(in: containerView)
         configureScrollViewInsets()
