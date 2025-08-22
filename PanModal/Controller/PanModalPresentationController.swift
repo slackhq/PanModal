b83c2bb9d98eff391dfc1e82c72929fc3d9fe1fa
Choshim.Wei <choshim@sidus.link>
2025-08-22 18:15:52 +0800

    fix：修复面板容器可能不显示问题

--- PanModal/Controller/PanModalPresentationController.swift
+++ PanModal/Controller/PanModalPresentationController.swift
@@ -176,7 +176,11 @@ open class PanModalPresentationController: UIPresentationController {
 
         guard let containerView = containerView
             else { return }
-
+        
+        if self.panContainerView.frame == .zero {
+            self.adjustPresentedViewFrame()
+        }
+        
         layoutBackgroundView(in: containerView)
         layoutPresentedView(in: containerView)
         configureScrollViewInsets()
